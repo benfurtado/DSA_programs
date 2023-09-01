a58352c531ca85e8931819be5d31fd88119ce34f
benfurtado <55623842+benfurtado@users.noreply.github.com>
2023-09-01 09:28:36 +0530

    Update 19_05_Ben.c

--- 19_05_Ben.c
+++ 19_05_Ben.c
@@ -6,25 +6,25 @@ int main()
 
 int size;
 
-	printf("enter no of elements in an array\n");	//enter input for array size
+	printf("enter no of elements in an array\n");	
 	
 	scanf("%d",&size);
 	
 int arr[size];
 	for(int i=0;i<=(size-1);i++)
 	{
-		printf("enter element for base index [%d]: ",i ); //enter elements of array
+		printf("enter element for base index [%d]: ",i ); 
 		scanf("%d",&arr[i]);
 	}
 
 	for(int i=0;i<=(size-1);i++)
 	{
-		printf("element for base index [%d] is: %d ",i,arr[i]); //displaying elements of all base index of an array
+		printf("element for base index [%d] is: %d ",i,arr[i]); 
 		printf("\n");
 	}
 	for(int i=0;i<=(size-1);i++)
 	{
-		if(arr[0]<arr[i])					//if arr[0] is less than the other assinging the element of that array to arr[0];
+		if(arr[0]<arr[i])				
 		{	
 			arr[0]=arr[i];
 			
